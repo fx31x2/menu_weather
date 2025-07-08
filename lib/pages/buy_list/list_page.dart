24b5368018053c2e37b32ea00843485cbcd6623b
tatsunori <kd1348940@st.kobedenshi.ac.jp>
2025-07-08 11:57:37 +0900

    za

--- lib/pages/buy_list/list_page.dart
+++ lib/pages/buy_list/list_page.dart
@@ -2,6 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:menu_weather/utils/utils.dart';
 import 'package:menu_weather/main.dart';
 
+
+
+class JapaneseMlbPlayer {
+   String name;
+   String team;
+
+   JapaneseMlbPlayer(this.name, this.team);
+}
+
 class ListPage extends StatefulWidget {
   const ListPage({super.key});
 
@@ -10,57 +19,74 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
-  
-  Map<String?, dynamic>map={
+
+
+
+  Map<String, dynamic>map={
     'ingredients' : [
       {
-        'name' : 'potato',
-        'price' : 100,
+        'name' : 'じゃがいも',
+        // 'price' : 100,
         'amount' : '中サイズ1個',
         'checkbox' : false,
       },
       {
-        'name' : 'carrot',
-        'price' : 120,
+        'name' : 'にんじん',
+        // 'price' : 120,
         'amount' : '1/4本',
         'checkbox' : false,
       },
       {
-        'name' : 'cucumber',
-        'price' : 100,
+        'name' : 'きゅうり',
+        // 'price' : 100,
         'amount' : '1/4本',
         'checkbox' : false,
       },
       {
-        'name' : 'ham',
-        'price' : 150,
+        'name' : 'ハム',
+        // 'price' : 150,
         'amount' : '1~2枚',
         'checkbox' : false,
       },
       {
-        'name' : 'mayonnaise',
-        'price' : 200,
+        'name' : 'マヨネーズ',
+        // 'price' : 200,
         'amount' : '大さじ1~2',
         'checkbox' : false,
       },
       {
-        'name' : 'salt',
-        'price' : 150,
+        'name' : '塩',
+        // 'price' : 150,
         'amount' : '少々',
         'checkbox' : false,
       },
       {
-        'name' : 'pepper',
-        'price' : 150,
+        'name' : '胡椒',
+        // 'price' : 150,
         'amount' : '少々',
         'checkbox' : false,
       }
+
     ]
   };
+
+  late List<Map<String, dynamic>> list;
+
+  @override
+  void initState() {
+    list = List<Map<String, dynamic>>.from(map['ingredients']);
+    super.initState();
+  }
+
+
   
+
   @override
   Widget build(BuildContext context) {
+    list = List<Map<String, dynamic>>.from(map['ingredients']);
+
     return Scaffold(
+      
       // 背景色
       backgroundColor: Colors.grey,
       body: Center(
@@ -76,7 +102,7 @@ class _ListPageState extends State<ListPage> {
                 ),
                 width: screenWidth(context)*0.8,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20)                  
+                  borderRadius: BorderRadius.circular(20)
                 ),
                 child: Text('ポテトサラダ',
                   style: TextStyle(
@@ -95,48 +121,106 @@ class _ListPageState extends State<ListPage> {
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(20)
               ),
-              
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: 
+              Column(
                 children: [
-                  Container(
-                    width: screenWidth(context)*0.3,
-                    child: Text(map['ingredients'][0]['name'],
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,                    
+                    children: [
+                      Container(
+                        width: screenWidth(context)*0.2,
+                        margin: EdgeInsets.only(right: 30, bottom: 20),
+                        height: 30,
+                        child: Text('材料',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
-                    )
-                  ),
-                  Container(
-                    width: screenWidth(context)*0.3,
-                    child: Text((map['ingredients'][0]['price']).toString(),
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold
+                      Container(
+                        width: screenWidth(context)*0.2,
+                        margin: EdgeInsets.only(right: 30),                        
+                        height: 30,
+                        child: Text('量',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
-                    )
+                      Container(
+                        height: 30,
+                        child: Text('　',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                  
+                    //   Container(
+                    //     width: screenWidth(context)*0.25,
+                    //     height: screen,
+                    //     padding: EdgeInsets.only(top: 10),
+                    //   ),
+                    ],
                   ),
-                  Container(
-                    width: screenWidth(context)*0.1,
-                    child: Checkbox(value: map['ingredients'][0]['checkbox'],
-                     onChanged: (value){
-                      setState(() {
-                        map['ingredients'][0]['checkbox'] = value!;
-                        print(map['ingredients'][0]['checkbox']);
-                      });
-                     }),
-                  )
-                ],
-                
-              )
 
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: list.length,
+                      itemBuilder: (context, index){
+                        final item = list[index];
+                        return ListTile(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: screenWidth(context)*0.25,
+                                padding: EdgeInsets.only(top: 10),
+                                child: Text(item['name'],
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                )
+                              ),
+                              Container(
+                                width: screenWidth(context)*0.25,
+                                padding: EdgeInsets.only(top: 10),
+                                child: Text(item['amount'],
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                )
+                              ),
+                              Checkbox(value: item['checkbox'],
+                                onChanged: (value){
+                                  setState(() {
+                                    map['ingredients'][index]['checkbox'] = value!;
+                                    print('$index : ${item['checkbox']}');
+                                  });
+                                }
+                              )                     
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ]
         ),
       )
-      
-
     );
   }
 }
